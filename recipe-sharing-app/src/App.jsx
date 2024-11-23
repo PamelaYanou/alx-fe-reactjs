5297adc77b3be8d81688d75803d0cbafff4864c6
PamelaYanou <yanoupamela@gmail.com>
2024-11-23 19:26:58 +0100

    update recipeStore.js

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-
+import recipeStore from './components/'
 const App = () => {
     return (
         <div>
             <h1>Recipe Sharing App</h1>
             <AddRecipeForm />
             <RecipeList />
+            <recipeStore/>
         </div>
     );
 };
