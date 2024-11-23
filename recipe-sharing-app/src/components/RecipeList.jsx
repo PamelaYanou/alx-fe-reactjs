cc32a82b3837eb2c99fa9fb6919325bd03b1c9aa
PamelaYanou <yanoupamela@gmail.com>
2024-11-23 19:44:42 +0100

    Update files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRecipeStore } from "./recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -12,6 +13,7 @@ const RecipeList = () => {
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
+            <Link to={`/recipe/${recipe.id}`}>View Details</Link>
           </div>
         ))
       ) : (
