baf78badff1dcf13282a9cf24b1cdd759a92af0a
PamelaYanou <yanoupamela@gmail.com>
2024-11-24 11:55:28 +0100

    create Login.jsx, ProtectedRoute.jsx,Profile.jsx,ProfileDetails.jsx,ProfileSettings.jsx and Post.jsx files

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const isAuthenticated = false;
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/post/:postId" element={<Post />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
